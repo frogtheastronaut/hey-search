6987f8b6125551b9e93cc9a7c206412111fdd5e6
Ethan Zhang <ethanzhangyixuan@gmail.com>
2025-08-02 11:02:25 +1000

    Big, big multitasking commit

--- crawler/join_crawlres.py
+++ crawler/join_crawlres.py
@@ -1,38 +1,18 @@
 # join crawlres.json from each of the folders
 import json
 
-# List of folder names
+# List of folder names (bot_1 through bot_50)
 FOLDER_NAMES = [
-    "alfa",
-    "bravo",
-    "charlie",
-    "delta",
-    "echo",
-    "foxtrot",
-    "golf",
-    "hotel",
-    "india",
-    "juliet",  
-    "kilo",
-    "lima",
-    "mike",
-    "november",
-    "oscar",
-    "papa",
-    "quebec",
-    "romeo",
-    "sierra",
-    "tango",
-    "uniform",
-    "victor",
-    "whiskey",
-    "xray",
-    "yankee",
-    "zulu"
+    "bot_1", "bot_2", "bot_3", "bot_4", "bot_5", "bot_6", "bot_7", "bot_8", "bot_9", "bot_10",
+    "bot_11", "bot_12", "bot_13", "bot_14", "bot_15", "bot_16", "bot_17", "bot_18", "bot_19", "bot_20",
+    "bot_21", "bot_22", "bot_23", "bot_24", "bot_25", "bot_26", "bot_27", "bot_28", "bot_29", "bot_30",
+    "bot_31", "bot_32", "bot_33", "bot_34", "bot_35", "bot_36", "bot_37", "bot_38", "bot_39", "bot_40",
+    "bot_41", "bot_42", "bot_43", "bot_44", "bot_45", "bot_46", "bot_47", "bot_48", "bot_49", "bot_50"
 ]
 
 combined_data = []
 combined_data_processed = []
+
 if __name__ == "__main__":
     # Get crawlres.json content from each folder
     for folder in FOLDER_NAMES:
@@ -40,21 +20,22 @@ if __name__ == "__main__":
             with open(f"{folder}/crawlres.json", 'r') as file:
                 data = json.load(file)
                 combined_data.append(data)
+                print(f"wrote data from folder {folder}")
         except FileNotFoundError:
-            print(f"Folder '{folder}' does not exist/crawlres.json missing")
+            print(f"folder '{folder}' does not exist/crawlres.json missing")
         except json.JSONDecodeError:
-            print(f"File in folder '{folder}' has no valuable information")
+            print(f"file in folder '{folder}' has no valuable information")
 
     # combine data
     for data in combined_data:
-        # Site known data URL -> site_known_data[0]
-        # Site known data DATA -> site_known_data[1]
-
-        # If URL not in combined_data_processed['url']
-        if data['site_known_data'] not in combined_data_processed:
-            for item in data['site_known_data']:
-                if item not in combined_data_processed:
-                    combined_data_processed.append(item)
+        # each data format is a dictionary:
+        # "url": "data"
+        for item in data['site_known_data']:
+            if item not in combined_data_processed:
+                combined_data_processed.append({
+                    "url": item,
+                    "data": data['site_known_data'][item]
+                })
 
     # put combined_data_processed into crawlres.json
     with open("crawlres.json", 'w') as file:
--- crawler/src/crawler.py
+++ crawler/src/crawler.py
@@ -2,55 +2,50 @@ import requests
 import json
 import time
 import sys, os
-from bs4 import BeautifulSoup # type: ignore
+from bs4 import BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor
 import time
 import random
+import threading
 
 
-# known websites that have not yet been crawled through
-known_websites = []
-known_website_data = []
-checking_websites = [] # websites crawler is checking
-
-# websites crawler has checked
-checked_websites = []
-failed_url_requests = []
+known_websites = set()
+known_website_data = {} 
+checking_websites = set()
+checked_websites = set()
+failed_url_requests = set()
 crawls = 0
 last_url_requested = ""
-stop = False
+stop = False # whether the crawler should stop
 
+# website locks
+websites_lock = threading.Lock()
+data_lock = threading.Lock()
+crawls_lock = threading.Lock()
 
 KNOWN_WEBSITE_SCHEMES = (
     "https://",
     "http://"
 )
+KNOWN_WEBSITE_DOMAINS = (
+    "com",
+    "org",
+    "net",
+    "edu",
+    "gov",
+    "mil",
+    "int",
+
+    "io",
+    "co",
+)
 
 STATUS_CODE_PASS = [
-    200,
-    201,
-    202,
-    203,
-    204,
-    205,
-    206,
-    207,
-    208,
-    226,
-    300,
-    301,
-    302,
-    303,
-    304,
-    305,
-    306,
-    307,
-    308,
-    100,
-    101,
-    102,
-    103
+    200, 201, 202, 203, 204, 205, 206, 207, 208, 226,
+    300, 301, 302, 303, 304, 305, 306, 307, 308,
+    100, 101, 102, 103
 ]
+
 class Colors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -69,105 +64,151 @@ class Crawler:
         global known_websites
         # init vars
         self.repeats = 0
-        self.read_database()
         self.website_keywords = ""
         self.cwd = os.getcwd()
+        self.read_database()
 
     def read_json(self, filename='crawlres.json'):
-        full_path = os.path.abspath(filename)
-        with open(f"{self.cwd}/{filename}", 'r') as file:
+        filepath = os.path.join(self.cwd, filename)
+        print(f"reading JSON from: {filepath}")
+        
+        with open(filepath, 'r') as file:
             res = json.load(file)
             # print first 2 items
             if 'site_known' in res and len(res['site_known']) > 0:
-                print(f"first entries {res['site_known'][:2]}")
-            print(f"full path of file: {full_path}")
+                print(f"first entries: {list(res['site_known'])[:2]}")
             return res
 
-    def write_json(self, new_data, filename='crawlres.json'):
+    def write_json(self, filename='crawlres.json'):
+        with websites_lock:
+            sites_known = list(known_websites)
+            sites_checked = list(checked_websites)
+        
+        with data_lock:
+            sites_data = known_website_data.copy()
+        
+        data = {
+            "site_known": sites_known,
+            "site_known_data": sites_data,
+            "site_checked": sites_checked,
+        }
+        
         with open(f"{self.cwd}/{filename}", "w") as outfile:
-            json.dump(new_data, outfile, indent=4)
+            json.dump(data, outfile, indent=4)
         print(Colors.OKGREEN + f"wrote to {self.cwd}/{filename}" + Colors.ENDC)
+
     def get_site_info(self, url):
-        global crawls
-        global known_websites
-        global last_url_requested
-        if url in checked_websites or url in failed_url_requests:
-            print(Colors.BOLD + Colors.WARNING + f"{url} already checked ##" + Colors.ENDC)
-            if crawls == 0:
-                print("db seems to have data. crawling last...")
-                self.get_site_info(known_websites[-1])
-            return 0
+        if stop == True:
+            print(Colors.OKCYAN + "stopping crawler bot..." + Colors.ENDC)
+            return
+        global crawls, known_websites, last_url_requested
+        # process URL first
+        url = self.process_url(url)
+        
+        with websites_lock:
+            if url in checked_websites or url in failed_url_requests:
+                print(Colors.BOLD + Colors.WARNING + f"{url} already checked ##" + Colors.ENDC)
+                return 0
+            
+            if url in checking_websites:
+                print(Colors.WARNING + f"already checking {url}, skipping..." + Colors.ENDC)
+                return
+            
+            checking_websites.add(url)
+
         links_in_site = []
         try:
-            # get the site info
-            if url in checking_websites:
-                print(Colors.WARNING + f"already checked {url}, skipping..." + Colors.ENDC)
-                return
-            checking_websites.append(url)
             print(f"getting url {url}...")
             last_url_requested = url
-            request = requests.get(url)
-            print(Colors.OKBLUE + "url recieved" + Colors.ENDC)
+            request = requests.get(url, timeout=10)
+            print(Colors.OKBLUE + "url received" + Colors.ENDC)
+            
             soup = BeautifulSoup(request.text, 'html.parser')
             for link in soup.find_all('a'):
-                links_in_site.append(link.get('href'))  
-            #print(f"{url} links recieved")
+                href = link.get('href')
+                if href:
+                    links_in_site.append(href)
+            
             for link2 in links_in_site:
-                # link2 has nothing in it
+                if stop == True:
+                    print(Colors.OKCYAN + "stopping crawler bot..." + Colors.ENDC)
+                    return
                 if link2 == None or len(link2) == 0:
-                    pass
+                    continue
+                
+                if link2.startswith(KNOWN_WEBSITE_SCHEMES):
+                    real_link = link2
                 else:
-                    if link2.startswith(KNOWN_WEBSITE_SCHEMES):
-                        # starts with eg. https://
-                        real_link = link2
-                    else:
-                        # eg. /google/website instead of google.com/google/website
-                        real_link = url + link2
+                    real_link = url + link2
+                real_link = self.process_url(real_link)
+                
+                with websites_lock:
                     if real_link in checked_websites or real_link in known_websites or real_link in failed_url_requests:
                         print(Colors.BOLD + real_link + Colors.WARNING + " already checked. skipping..." + Colors.ENDC)
-                        pass
+                        continue
+                    
+                    if real_link in checking_websites:
+                        print(Colors.WARNING + f"already checking {real_link}, skipping.." + Colors.ENDC)
+                        continue
+                    
+                    checking_websites.add(real_link)
+                
+                try:
+                    print(f"requesting found link")
+                    last_url_requested = real_link
+                    href_request = requests.get(real_link, timeout=5)
+                    
+                    if href_request.status_code in STATUS_CODE_PASS:
+                        print(Colors.OKBLUE + f"request success: {real_link}" + Colors.ENDC)
+                        sub_soup = BeautifulSoup(href_request.text, 'html.parser')
+                        
+                        with websites_lock:
+                            known_websites.add(real_link)
+                            checking_websites.discard(real_link)
+                        # these are the keywords we want to extract
+                        og = self.get_og(sub_soup)
+                        keywords = self.get_site_keywords(self.get_url_keywords(real_link), og['site'], og['title'], og['desc'], og['site_content'])
+
+                        with data_lock:
+                            # put the keywords into known_website_data
+                            known_website_data[real_link] = keywords
                     else:
-                        if real_link in checking_websites:
-                            # already checking this link
-                            print(Colors.WARNING + f"already checked {real_link}, skipping.." + Colors.ENDC)
-                            known_websites.append(real_link)
-                            pass
-                        else:
-                            # request real link
-                            print(f"requesting found link")
-                            last_url_requested = real_link
-                            href_request = requests.get(real_link)
-                            if href_request.status_code in STATUS_CODE_PASS:
-                                print(Colors.OKBLUE + f"request success: {real_link}" + Colors.ENDC)
-                                sub_soup = BeautifulSoup(href_request.text, 'html.parser')
-                                known_websites.append(real_link)
-                                og = self.get_og(sub_soup)
-                                keywords = self.get_site_keywords(real_link, og['site'], og['title'], og['desc'], og['site_content'])
-                                known_website_data.append(
-                                    [real_link, keywords]
-                                )
-                            else:
-                                print(Colors.FAIL + f"request failed: {real_link}" + Colors.ENDC)
-                                failed_url_requests.append(real_link)
-            checked_websites.append(url)
+                        print(Colors.FAIL + f"request failed: {real_link}" + Colors.ENDC)
+                        with websites_lock:
+                            failed_url_requests.add(real_link)
+                            checking_websites.discard(real_link)
+                            
+                except Exception:
+                    with websites_lock:
+                        failed_url_requests.add(real_link)
+                        checking_websites.discard(real_link)
+            
+            with websites_lock:
+                checked_websites.add(url)
+                checking_websites.discard(url)
+            
             self.repeats = 0
             return links_in_site
+            
         except Exception as e:
-            if e == requests.exceptions.ConnectionError:
+            with websites_lock:
+                checking_websites.discard(url)
+            
+            if isinstance(e, requests.exceptions.ConnectionError):
                 print(Colors.FAIL + "connection error" + Colors.ENDC)
-                if self.repeats > 3:
-                    print(Colors.FAIL + "retries exceeded. breaking." + Colors.ENDC)
-                    failed_url_requests.append(last_url_requested)
-                    self.repeats = 0
-                else:
-                    print(Colors.WARNING + "connection failed. sleeping..." + Colors.WARNING)
-                    time.sleep(1)
-                    print(Colors.WARNING +"reconnecting to last url..." + Colors.WARNING)
+                if self.repeats < 3:
                     self.repeats += 1
-                    self.get_site_info(last_url_requested)
+                    time.sleep(1)
+                    return self.get_site_info(url)
+                else:
+                    with websites_lock:
+                        failed_url_requests.add(last_url_requested)
+                    self.repeats = 0
             else:
-                failed_url_requests.append(last_url_requested)
-                self.get_site_info(url)
+                with websites_lock:
+                    failed_url_requests.add(last_url_requested)
+            return []
+
     def get_og(self, soup):
         # gets the open graph meta information of a soup
         title = soup.find("meta",  property="og:title")
@@ -179,20 +220,31 @@ class Crawler:
             "desc":  None if desc == None or 'content' not in desc else desc['content'],
             "site_content": self.get_site_content(soup)
         }
+
     def get_site_content(self, soup):
         [s.extract() for s in soup(['style', 'script', '[document]', 'head', 'title'])]
         return soup.get_text(separator=' ', strip=True)
+
     def read_database(self):
-        global known_website_data
-        global known_websites
-        global checked_websites
+        global known_website_data, known_websites, checked_websites
         try:
             json_data = self.read_json()
-            known_website_data = json_data["site_known_data"]
-            known_websites = json_data["site_known"]
-            checked_websites = json_data["site_checked"]
+            
+            with data_lock:
+                # Handle both old list format and new dict format
+                if isinstance(json_data.get("site_known_data", []), list):
+                    # Convert old format [url, keywords] to dict
+                    known_website_data = {item[0]: item[1] for item in json_data["site_known_data"] if len(item) >= 2}
+                else:
+                    known_website_data = json_data.get("site_known_data", {})
+            
+            with websites_lock:
+                known_websites = set(json_data.get("site_known", []))
+                checked_websites = set(json_data.get("site_checked", []))
+                
         except:
-            print(Colors.WARNING + "warning: json file has not valuable information" + Colors.ENDC)
+            print(Colors.WARNING + "warning: json file has no valuable information" + Colors.ENDC)
+
     def get_site_keywords(self, url, site, title, desc, site_content):
         website_keywords = ""
         keywords_list = []
@@ -215,43 +267,108 @@ class Crawler:
         processed_str = ' '.join(keywords_processed)
         return processed_str
 
+    def process_url(self, url):
+        # remove excess slashes, eg. https://example.com//path//to//resource to https://example.com/path/to/resource
+        segments = url.split('/')
+        correct_segments = []
+        for segment in segments:
+            if segment != '':
+                correct_segments.append(segment)
+        first_segment = str(correct_segments[0])
+        if first_segment.find('http') == -1:
+            correct_segments = ['http:'] + correct_segments
+        correct_segments[0] = correct_segments[0] + '/'
+        normalized_url = '/'.join(correct_segments)
+
+        # then remove ? mark
+        if '?' in normalized_url:
+            normalized_url = normalized_url.split('?')[0]
+        # then remove # mark
+        if '#' in normalized_url:
+            normalized_url = normalized_url.split('#')[0]
+        # remove trailing slash
+        if normalized_url.endswith('/'):
+            normalized_url = normalized_url[:-1]
+        return normalized_url
+    def get_url_keywords(self, url):
+        # remove non ascii characters in url
+        for char in url:
+            if char not in 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789':
+                url = url.replace(char, ' ')
+        
+        # split url into small sections
+        url_sections = url.split(' ')
+        
+        filtered_sections = []
+        for section in url_sections:
+            section = section.strip()  # remove leading and trailing spaces
+            
+            if (section == "https" or 
+                section == "http" or 
+                section == "www" or 
+                section in KNOWN_WEBSITE_DOMAINS or 
+                section == ""):
+                # skip
+                continue
+            else:
+                filtered_sections.append(section)
+
+        # ok thats about it
+        return ' '.join(filtered_sections).strip()  # remove leading and trailing spaces
+
+
 class CrawlBot:
     def __init__(self):
         self.crawler = Crawler()
-        pass
+
     def crawl(self):
         global crawls
         print(Colors.HEADER + "initiating crawl.." + Colors.ENDC)
-        for website in known_websites:
-            if website in checked_websites or website in failed_url_requests:
-                print(Colors.BOLD + Colors.WARNING + f"{website} already checked" + Colors.ENDC)
-                if crawls == 0 and len(known_websites) > 1:
-                    print("db seems to have data. crawling last...")
-                    self.crawler.get_site_info(known_websites[-1])
-                    #time.sleep(0.31)
-                return
-            else:
+        
+        while not stop:
+            website = None
+            
+            with websites_lock:
+                uncrawled = known_websites - checked_websites - failed_url_requests
+                if uncrawled:
+                    website = random.choice(list(uncrawled))
+            
+            if website:
+                with websites_lock:
+                    if website in checked_websites or website in failed_url_requests:
+                        continue
+                
                 print(f"crawling {website}")
                 self.crawler.get_site_info(website)
-                self.crawler.write_json(
-                    {
-                        "site_known": known_websites,
-                        "site_known_data": known_website_data,
-                        "site_checked": checked_websites,
-                    }
-                )
-                #print(Colors.OKGREEN + f"wrote to {self.crawler.cwd}/{self.crawler.filename}" + Colors.ENDC)
-                crawls += 1
-# sleep_times is 50 prime numbers
-sleep_times = [0.31, 0.32, 0.33, 0.34, 0.35, 0.36, 0.37, 0.38, 0.39, 0.40, 0.41, 0.43, 0.44, 0.45, 0.46, 0.47, 0.49, 0.50, 0.53, 0.59, 0.61, 0.67, 0.71, 0.73, 0.79, 0.83, 0.89, 0.97, 0.101, 0.103, 0.107, 0.109, 0.113, 0.127, 0.131, 0.137, 0.139, 0.149, 0.151, 0.157, 0.163, 0.167, 0.173, 0.179, 0.181, 0.191, 0.193, 0.197, 0.199, 0.211, 0.223, 0.227, 0.229, 0.233, 0.239, 0.241, 0.251, 0.257, 0.263, 0.269, 0.271, 0.277, 0.281, 0.283, 0.293]
+                
+                with crawls_lock:
+                    crawls += 1
+                    current_crawls = crawls
+                
+                # write to json every 10 crawls
+                if current_crawls % 10 == 0:
+                    self.crawler.write_json()
+
+                time.sleep(0.1)
+            else:
+                time.sleep(1)
+        print(Colors.OKCYAN + "this process stopped. bye!" + Colors.ENDC)
+
 if __name__ == "__main__":
-    known_websites = [sys.argv[1] if len(sys.argv) > 1 else "https://google.com"]
+    initial_site = sys.argv[1] if len(sys.argv) > 1 else "https://google.com"
+    with websites_lock:
+        known_websites.add(initial_site)
+    
     crawlbot = CrawlBot()
-    futures = []
-    with ThreadPoolExecutor(max_workers=50) as executor:
-        for _ in range(50):
-            executor.submit(crawlbot.crawl)
-            #time.sleep(random.choice(sleep_times))
-            sleep_time = random.choice(sleep_times)
-            time.sleep(sleep_time) # avoid rush hour traffic
-            sleep_times.remove(sleep_time) # remove used sleep time
+    
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        futures = [executor.submit(crawlbot.crawl) for _ in range(10)]
+        
+        try:
+            for future in futures:
+                future.result()
+        except KeyboardInterrupt:
+            stop = True
+            print(Colors.OKCYAN + "stopping crawler..." + Colors.ENDC)
+            print(Colors.OKCYAN + "writing to json file one last time..." + Colors.ENDC)
+            crawlbot.crawler.write_json()
